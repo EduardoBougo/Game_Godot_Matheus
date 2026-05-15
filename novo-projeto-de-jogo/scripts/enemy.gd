2387b953c2d92a40c5912c42f5ecf0bd8557477c
EduardoBougo <eduardobougo@gmail.com>
2026-05-14 23:46:26 -0300

    sistema de eliminaao de inimigo

--- novo-projeto-de-jogo/scripts/enemy.gd
+++ novo-projeto-de-jogo/scripts/enemy.gd
@@ -8,14 +8,18 @@ extends CharacterBody2D
 var direction : Vector2 = Vector2.ZERO
 var player = null
 
+var eliminado = false
+
 func _ready() -> void:
 	player = Global.player
 
 
 func _physics_process(delta: float) -> void:
-	direction = global_position.direction_to(player.global_position)
-	velocity = direction * move_speed
-	move_and_slide()
+	if eliminado == false:
+		direction = global_position.direction_to(player.global_position)
+		velocity = direction * move_speed
+		move_and_slide()
+		
 
 # --- Função para Receber Dano--- 
 func take_damage(amount: int, source_position: Vector2):
@@ -25,6 +29,7 @@ func take_damage(amount: int, source_position: Vector2):
 	animated_sprite_2d.play("hit")
 	
 	if health <= 0:
+		eliminado = true
 		animated_sprite_2d.play("dead")
 		await get_tree().create_timer(1.5).timeout
 		queue_free()
--- novo-projeto-de-jogo/scripts/fire_ball.gd
+++ novo-projeto-de-jogo/scripts/fire_ball.gd
@@ -10,5 +10,6 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if(!body.is_in_group("player")): # Mudar a verificação quando tiver inimigos
+	if (body.is_in_group("enemies")):
+		body.take_damage(1, global_position)
 		queue_free();
