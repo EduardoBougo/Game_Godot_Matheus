7cfee132bb36f797658697b11b4cc9dcda5d9006
EduardoBougo <eduardobougo@gmail.com>
2026-05-14 22:52:49 -0300

    Cooldown

--- novo-projeto-de-jogo/fire_ball.gd
+++ novo-projeto-de-jogo/fire_ball.gd
@@ -7,3 +7,8 @@ var direction := Vector2.ZERO;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta;
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if(!body.is_in_group("player")): # Mudar a verificação quando tiver inimigos
+		queue_free();
